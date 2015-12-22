09ad699803ee9bbde5c3ac3960fb4fe1021456f6
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-12-22 13:35:50 -0500

    [dd-tcp-rtt] Changing dependency name to datadog-rtt.

--- config/software/datadog-rtt.rb
+++ config/software/datadog-rtt.rb
@@ -1,9 +1,9 @@
-name "dd-tcp-rtt"
+name "datadog-rtt"
 default_version "last-stable"
 
 env = {
   "GOROOT" => "/usr/local/go",
-  "GOPATH" => "/var/cache/omnibus/src/dd-tcp-rtt"
+  "GOPATH" => "/var/cache/omnibus/src/datadog-rtt"
 }
 
 if ohai['platform_family'] == 'mac_os_x'
@@ -16,6 +16,6 @@ end
 build do
    ship_license "https://raw.githubusercontent.com/DataDog/dd-tcp-rtt/master/LICENSE"
    command "#{gobin} get -d -u github.com/DataDog/dd-tcp-rtt", :env => env
-   command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/dd-tcp-rtt/src/github.com/DataDog/dd-tcp-rtt"
+   command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
    command "#{gobin} build -o #{install_dir}/bin/dd-tcp-rtt $GOPATH/src/github.com/DataDog/dd-tcp-rtt", :env => env
 end
