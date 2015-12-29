fe0093f25205376f35cc7550f53e0b211c8f2ba8
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-12-29 13:48:36 +0100

    [dd-tcp-rtt] rake in all dependencies - do not update.

--- config/software/datadog-rtt.rb
+++ config/software/datadog-rtt.rb
@@ -18,12 +18,10 @@ end
 
 build do
    ship_license "https://raw.githubusercontent.com/DataDog/dd-tcp-rtt/master/LICENSE"
-   # command "git config --global url.\"git@github.com:\".insteadOf \"https://github.com/\"", :env => env
-   # command "#{gobin} get -d -u github.com/DataDog/dd-tcp-rtt", :env => env
    command "mkdir -p /var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog", :env => env
    command "git clone https://#{github_token}:x-oauth-basic@github.com/DataDog/dd-tcp-rtt.git", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog"
    command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
-   command "#{gobin} get -d -u ./...", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
+   command "#{gobin} get -d ./...", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
    patch :source => "libpcap-static-link.patch", :plevel => 0, :target => "$GOPATH/src/github.com/google/gopacket"
    command "#{gobin} build -o #{install_dir}/bin/dd-tcp-rtt $GOPATH/src/github.com/DataDog/dd-tcp-rtt", :env => env
 end
