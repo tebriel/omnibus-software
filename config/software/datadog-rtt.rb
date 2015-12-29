c6ab30de4177f810fa5ce9592bb2d11ecee3c092
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-12-29 11:21:23 +0100

    [dd-tcp-rtt] Enable github access token for private repo access.

--- config/software/datadog-rtt.rb
+++ config/software/datadog-rtt.rb
@@ -1,5 +1,6 @@
 name "datadog-rtt"
 default_version "last-stable"
+github_token = ENV['GITHUB_TOKEN']
 
 env = {
   "GOROOT" => "/usr/local/go",
@@ -15,9 +16,12 @@ end
 
 build do
    ship_license "https://raw.githubusercontent.com/DataDog/dd-tcp-rtt/master/LICENSE"
-   command "git config --global url.\"git@github.com:\".insteadOf \"https://github.com/\"", :env => env
-   command "#{gobin} get -d -u github.com/DataDog/dd-tcp-rtt", :env => env
+   # command "git config --global url.\"git@github.com:\".insteadOf \"https://github.com/\"", :env => env
+   # command "#{gobin} get -d -u github.com/DataDog/dd-tcp-rtt", :env => env
+   command "mkdir -p /var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
+   command "git clone https://#{github_token}@github.com/DataDog/dd-tcp-rtt.git", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog"
    command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
+   command "#{gobin} get -d -u ./...", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
    patch :source => "libpcap-static-link.patch", :plevel => 0, :target => "$GOPATH/src/github.com/google/gopacket"
    command "#{gobin} build -o #{install_dir}/bin/dd-tcp-rtt $GOPATH/src/github.com/DataDog/dd-tcp-rtt", :env => env
 end
